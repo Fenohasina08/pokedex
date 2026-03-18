993ad93d9357ccc9d0c460ae8426cd6877b33022
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-18 19:43:10 +0300

    feat: add content of the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <div className="bg-[#e2c107] py-3">
+            <h1 className="text-center text-2xl font-bold text-white">Pokémon Search</h1>
+        </div>
+    )
+}
+export default Header
