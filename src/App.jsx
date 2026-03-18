b57e9346ce593b27626c4bca30bc32ceaf3366ff
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-18 20:01:34 +0300

    chore: remove unused code and comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Home from './components/Home'
+import Header from './components/Header'
+
 const App = () => {
     return (
-        <div className="bg-[#FCEE]">
+        <div className="bg-[#f5f5f5]">
             <Header/>
             <Home />
         </div>
